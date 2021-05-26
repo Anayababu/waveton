a983d6e6bab8bcdca160c7a611a7f17823c8e01d
Aleena-Carolin <76088086+Aleena-Carolin@users.noreply.github.com>
2021-05-26 14:03:17 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Destination
+from django.contrib.auth.models import User,auth
+
+
+# Create your views here.
+def logintr(request):
+    if request.method =='POST':
+        username= request.POST['name']
+        email= request.POST['email']
+        password1= request.POST['password1']
+        password2= request.POST['password2']
+
+        user = User.objects.create_user(username=username, email=email, password=password1)
+        user.save()
+        print('user created')
+        return redirect('tlogin/autovi.html')
+    else:
+        return render(request,'logintr.html')
